42577ad5472e29a9f4927f99f737fdf7aa73a744
yaswanthshiva <153359207+yaswanthshiva@users.noreply.github.com>
2026-03-14 10:53:12 +0530

    Time: 4 ms (81.45%), Space: 46.1 MB (48.64%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length())
+        {
+            return false;
+        }
+        char[] s1 = s.toCharArray();
+        char[] s2 = t.toCharArray();
+        Arrays.sort(s1);
+        Arrays.sort(s2);
+        return Arrays.equals(s1,s2);
+    }
+}
