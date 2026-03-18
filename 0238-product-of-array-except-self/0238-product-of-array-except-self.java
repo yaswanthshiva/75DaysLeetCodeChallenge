93fd23f50e49ed50c457c52569ea2b5762102efb
yaswanthshiva <153359207+yaswanthshiva@users.noreply.github.com>
2026-03-18 09:29:33 +0530

    Add comment to clarify logic in productExceptSelf

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
         int[] ans = new int[n];
-        ans[0] = 1;
+        ans[0] = 1;                                        // (((((Logic = left_product * right_product)))))
         for(int i=1;i<n;i++)
         {
             ans[i] = ans[i-1]*nums[i-1];
